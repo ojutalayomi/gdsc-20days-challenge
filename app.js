ab557afc107ddf8351a43aeb19aaf2d93441fca7
Ayomide <ojutalayoayomide21@gmail.com>
2024-04-03 16:27:59 +0100

    Created two api endpoints(/hello and /api/hello)

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ require('dotenv').config();
 const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
+const cors = require('cors');
+const bodyParser = require('body-parser');
 
 const app = express();
 
@@ -12,8 +14,19 @@ const join = require('path').join;
 const port = process.env.PORT;
 
 app.use(express.static(path.join(__dirname, '../public')));
+app.use(express.json());
+app.use(bodyParser.urlencoded({ extended: true, limit: '7mb' }));
 
+app.use(cors());
 app.use(express.static(__dirname));
+
+app.get('/hello', async (req,res) => {
+    res.send('<h1> <b>Hello GDSC</b> </h1>');
+})
+
+app.get('/api/hello', async (req,res) => {
+    res.status(200).json({message: 'Hello GDSC'});
+})
 
 // 
 app.listen(port, () => {
