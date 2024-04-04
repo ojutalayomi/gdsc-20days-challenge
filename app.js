a8eb4548f821850a67dddffe00251832ad332bf4
Ayomide <ojutalayoayomide21@gmail.com>
2024-04-04 17:22:31 +0100

    The bookstore api was created

--- app.js
+++ app.js
@@ -21,11 +21,79 @@ app.use(cors());
 app.use(express.static(__dirname));
 
 app.get('/hello', async (req,res) => {
+    res.send('<h1> <b>Hello GDSC</b> </h1>');
+});
+
+app.get('/api/hello', async (req,res) => {
+    res.status(200).json({message: 'Hello GDSC'});
+});
+
+app.get('/bookstore', async (req,res) => {
     try{
         await client.connect();
         console.log('Connected to database');
         const collection = client.db('gdsc').collection('db');
-        res.send('<h1> <b>Hello GDSC</b> </h1>');
+        const books = await collection.find({}).toArray();
+        res.status(200).json(books);
+        
+      } catch (err){
+        console.error('Error: ',err);
+        res.status(500).json({error: err});
+      } finally {
+        await client.close();
+      }
+})
+/*
+app.post('/bookstore', async (req,res) => {
+    try{
+        await client.connect();
+        console.log('Connected to database');
+        const collection = client.db('gdsc').collection('db');
+        const time = new Date().toLocaleString();
+
+        const updateFields = {
+            author: 'John Joe',
+            title: 'Chapter Of Life',
+            date_uploaded: time,
+            author_email: 'johnjoe@gmail.com',
+            book_cover: 'chapter-of-life.jpg',
+            verified: true,
+            isbn: "978-0-7432-7356-5",
+            publisher: "Life Publishers",
+            publication_date: "1925-04-10",
+            genres: ["Memoir", "Self-Help"],
+            description: "Chapter Of Life is a memoir written by John Joe that reflects on personal experiences, challenges, and lessons learned throughout life's journey.",
+            language: "English",
+            page_count: 180,
+            availability: true,
+            format: "Hardcover",
+            price: 24.99,
+            reviews: [
+                {
+                  user: "BookLover123",
+                  rating: 4.8,
+                  comment: "An inspiring and thought-provoking memoir. Highly recommended!"
+                },
+                {
+                    user: "ReadingFanatic",
+                    rating: 5,
+                    comment: "A beautifully written book that touches the heart. Couldn't put it down!"
+                }
+              ],
+              related_books: [
+                {
+                    title: "The Power of Now",
+                    author: "Eckhart Tolle"
+                },
+                {
+                    title: "Becoming",
+                    author: "Michelle Obama"
+                }
+              ]
+        };
+
+        // await collection.insertOne( updateFields );
+        console.log('Successfully added to the database')
 
     } catch (err){
         console.error('Error: ',err);
@@ -33,11 +101,7 @@ app.get('/hello', async (req,res) => {
     } finally {
         await client.close();
     }
-});
-
-app.get('/api/hello', async (req,res) => {
-    res.status(200).json({message: 'Hello GDSC'});
-});
+});*/
 
 app.get('/', async (req,res) => {
     res.redirect('/hello');
